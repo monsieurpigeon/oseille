ff8cc4a4691bfe21f9cdb200077bf79ab5d1844e
Maxime PIGEON <maxpige@gmail.com>
2022-11-29 00:45:59 +0100

    update Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,38 @@
 export function Home() {
-    return (
-        <div>
-            <h1>Accueil</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Bienvenue sur Oseille</h1>
+      <p>L'application qui aide les maraîchers à gérer leur argent</p>
+      <p>Application gratuite, en code libre, et qui marche sans internet</p>
+      <p>Fonctionnalités :</p>
+      <ul>
+        <li>Gérer une liste de produits avec un prix</li>
+        <li>Gérer une liste de clients</li>
+        <li>Changer les prix des produits facilement</li>
+        <li>Générer un bon de livraison</li>
+        <li>Calcul automatique des lignes de livraison</li>
+        <li>Grouper des bons de livraison en une facture</li>
+        <li>Export .pdf des bons de livraison et des factures</li>
+        <li>Import/Export de capsules temporelles pour protéger la data sur un autre support</li>
+        <li>... et ensuite ...</li>
+        <li>Gérer le status des documents (envoyé, payé)</li>
+        <li>Suivi des ventes par client et par produit</li>
+        <li>... Plein d'autres choses ...</li>
+        <li>Gestion des paniers en click-and-collect personnalisé</li>
+        <li>... Envoyez moi vos idées</li>
+      </ul>
+      <a
+        href="https://www.facebook.com/maxime.pigeon/"
+        target="_blank"
+      >
+        Facebook
+      </a>
+      <a
+        href="https://www.linkedin.com/in/maxime-pigeon/"
+        target="_blank"
+      >
+        Linkedin
+      </a>
+    </div>
+  );
 }
