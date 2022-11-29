67d5abe46b0a15487c223a938d9ba862a3e6f834
Maxime PIGEON <maxpige@gmail.com>
2022-11-29 01:05:05 +0100

    better Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,41 @@
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const StyledH1 = styled.h1`
+  font-size: 2rem;
+`;
+const StyledH2 = styled.h2`
+  font-size: 1.5rem;
+`;
+const StyledH3 = styled.h3``;
+
+const StyledSubtitle = styled.p``;
+
+const StyledUl = styled.ul`
+  margin-left: 50px;
+  list-style: none;
+`;
+
+const StyledLink = styled.a`
+  border-radius: 10px;
+  border: 1px solid #000;
+  padding: 10px;
+  margin: 20px;
+`;
+
 export function Home() {
   return (
-    <div>
-      <h1>Bienvenue sur Oseille</h1>
-      <p>L'application qui aide les maraîchers à gérer leur argent</p>
+    <StyledContainer>
+      <StyledH1>Bienvenue sur Oseille</StyledH1>
+      <StyledSubtitle>L'application qui aide les maraîchers à gérer leur argent</StyledSubtitle>
       <p>Application gratuite, en code libre, et qui marche sans internet</p>
-      <p>Fonctionnalités :</p>
-      <ul>
+      <StyledH2>Fonctionnalités :</StyledH2>
+      <StyledUl>
         <li>Gérer une liste de produits avec un prix</li>
         <li>Gérer une liste de clients</li>
         <li>Changer les prix des produits facilement</li>
@@ -20,19 +50,21 @@ export function Home() {
         <li>... Plein d'autres choses ...</li>
         <li>Gestion des paniers en click-and-collect personnalisé</li>
         <li>... Envoyez moi vos idées</li>
-      </ul>
-      <a
-        href="https://www.facebook.com/maxime.pigeon/"
-        target="_blank"
-      >
-        Facebook
-      </a>
-      <a
-        href="https://www.linkedin.com/in/maxime-pigeon/"
-        target="_blank"
-      >
-        Linkedin
-      </a>
-    </div>
+      </StyledUl>
+      <div>
+        <StyledLink
+          href="https://www.facebook.com/maxime.pigeon/"
+          target="_blank"
+        >
+          Facebook
+        </StyledLink>
+        <StyledLink
+          href="https://www.linkedin.com/in/maxime-pigeon/"
+          target="_blank"
+        >
+          Linkedin
+        </StyledLink>
+      </div>
+    </StyledContainer>
   );
 }
