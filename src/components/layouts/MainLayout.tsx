d09385dca271b3ad7fe32c2412d5308e371a89c5
Maxime PIGEON <maxpige@gmail.com>
2022-11-29 01:55:22 +0100

    update home

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -18,11 +18,15 @@ const StyledPage = styled.div`
 const StyledContent = styled.div`
   display: flex;
   flex-grow: 1;
+  background-color: silver;
 `;
 
 const StyledSreen = styled.main`
   flex-grow: 1;
   padding: 12px;
+  background-color: white;
+  border-radius: 20px 0 0 20px;
+  box-shadow: -6px -6px 10px 0 rgba(0, 0, 0, 0.2);
 `;
 
 export function MainLayout({ children }: { children: ReactNode }) {
