eba354f0a0c3234e744aa789858828bab618fb8f
monsieurpigeon <maxpige@gmail.com>
2022-12-04 00:09:17 +0100

    switch to pdfmake

--- src/backend/service/pdf.ts
+++ src/backend/service/pdf.ts
@@ -1,54 +1,73 @@
-import { BLANK_PDF, generate, Template } from '@pdfme/generator';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import { Product } from '../entity/product';
 
-const template: Template = {
-  basePdf: BLANK_PDF,
-  schemas: [
-    {
-      a: {
-        type: 'text',
-        position: { x: 0, y: 0 },
-        width: 10,
-        height: 10,
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
+
+const titles = {
+  Delivery: 'Bon de livraison',
+  Invoice: 'Facture',
+};
+
+type DocumentKey = 'Delivery' | 'Invoice';
+
+export const exportDocument = ({ payload }: any) => {
+  const title = titles[payload.type as DocumentKey] || '';
+  const docDefinition: any = {
+    footer: { text: `${title} genere gratuitement grace a Oseille`, alignment: 'center' },
+    content: [
+      { text: title, style: 'header' },
+      {
+        layout: 'noBorders',
+        style: 'tableExample',
+        table: {
+          widths: ['*', '*'],
+          body: [['Ferme A', { text: payload.customer.name, alignment: 'right' }]],
+        },
       },
-      b: {
-        type: 'text',
-        position: { x: 10, y: 10 },
-        width: 10,
-        height: 10,
+      {
+        layout: 'lightHorizontalLines',
+        style: 'tableExample',
+        table: {
+          headerRows: 1,
+          widths: ['*', '*', '*', '*'],
+          body: [
+            ['Produit', 'Prix', 'Quantite', 'Total'],
+            ...payload.products.map((el: { product: Product; quantity: number }) => {
+              return [el.product.name, el.product.price, el.quantity, el.product.price * el.quantity];
+            }),
+          ],
+        },
       },
-      c: {
-        type: 'text',
-        position: { x: 20, y: 20 },
-        width: 10,
-        height: 10,
+      {
+        text: `Total: ${payload.products.reduce((acc: number, el: { product: Product; quantity: number }) => {
+          return acc + el.product.price * el.quantity;
+        }, 0)}`,
+      },
+    ],
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true,
+        margin: [0, 0, 0, 10],
+      },
+      subheader: {
+        fontSize: 16,
+        bold: true,
+        margin: [0, 10, 0, 5],
+      },
+      tableExample: {
+        margin: [0, 5, 0, 15],
+      },
+      tableHeader: {
+        bold: true,
+        fontSize: 13,
+        color: 'black',
       },
     },
-  ],
+    defaultStyle: {
+      // alignment: 'justify'
+    },
+  };
+  pdfMake.createPdf(docDefinition).open();
 };
-export const exportDocument = ({ payload }: any) => {
-  console.log(payload);
-  const inputs = [{ a: 'a1', b: 'b1', c: 'c1' }];
-
-  generate({ template, inputs }).then((pdf) => {
-    console.log(pdf);
-    const blob = new Blob([pdf.buffer], { type: 'application/pdf' });
-    window.open(URL.createObjectURL(blob));
-  });
-};
-// if (payload.type === 'Delivery') {
-//   doc.text('Bon de livraison', 10, 10);
-// } else if (payload.type === 'Invoice') {
-//   doc.text('Facture', 10, 10);
-// }
-//
-// doc.text(payload.customer.name, 10, 20);
-// const totals: number[] = payload.products.map((el: any, index: number) => {
-//   const total = el.quantity * el.product.price;
-//   doc.text(`${el.product.name} - ${el.quantity} - ${el.product.price} : ${total}`, 10, 30 + 10 * index);
-//   return total;
-// });
-//
-// doc.text(`Total:  ${totals.reduce((acc, total) => acc + total, 0)}`, 10, 200);
-//
-// doc.text('genere gratuitement grace a Oseille', 10, 285);
-// doc.save('a4.pdf');
