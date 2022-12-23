340d0f9f8906c0cf7011016ed886d4354d98bea7
Maxime PIGEON <maxpige@gmail.com>
2022-12-23 02:32:41 +0100

    add myofff

---
+++ lib/myofff/src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import './App.css';
+import reactLogo from './assets/react.svg';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a
+          href="https://vitejs.dev"
+          target="_blank"
+        >
+          <img
+            src="/vite.svg"
+            className="logo"
+            alt="Vite logo"
+          />
+        </a>
+        <a
+          href="https://reactjs.org"
+          target="_blank"
+        >
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+          />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        This will be a totally blank page where you will be able to find more and more personnalisations. A sort of a
+        notebook of the future. What do you want to note offline ? I use it to build my library for offline-first apps.
+      </p>
+    </div>
+  );
+}
+
+export default App;
