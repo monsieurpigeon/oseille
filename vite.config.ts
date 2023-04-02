718128850903b46e80a435854532db7f46f99066
Maxime PIGEON <maxpige@gmail.com>
2023-04-02 23:56:30 +0200

    fix offline pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,71 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { ManifestOptions, VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+import replace from '@rollup/plugin-replace';
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: 'development',
+  base: '/',
+  includeAssets: ['favicon.svg'],
+  manifest: {
+    name: 'PWA Router',
+    short_name: 'PWA Router',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-192x192.png', // <== don't add slash, for testing
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/pwa-512x512.png', // <== don't remove slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png', // <== don't add slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === 'true',
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+    navigateFallback: 'index.html',
+  },
+};
+
+const replaceOptions = { __DATE__: new Date().toISOString() };
+const claims = process.env.CLAIMS === 'true';
+const reload = process.env.RELOAD_SW === 'true';
+const selfDestroying = process.env.SW_DESTROY === 'true';
+
+if (process.env.SW === 'true') {
+  pwaOptions.srcDir = 'src';
+  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts';
+  pwaOptions.strategies = 'injectManifest';
+  (pwaOptions.manifest as Partial<ManifestOptions>).name = 'PWA Inject Manifest';
+  (pwaOptions.manifest as Partial<ManifestOptions>).short_name = 'PWA Inject';
+}
+
+if (claims) pwaOptions.registerType = 'autoUpdate';
+
+if (reload) {
+  // @ts-expect-error just ignore
+  replaceOptions.__RELOAD_SW__ = 'true';
+}
+
+if (selfDestroying) pwaOptions.selfDestroying = selfDestroying;
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  build: {
+    sourcemap: process.env.SOURCE_MAP === 'true',
+  },
+  plugins: [react(), VitePWA(pwaOptions), replace(replaceOptions)],
   define: {
     global: 'window',
   },
