c71d0c8ab1f624b588000eeada4f65f9f4d5dcf4
Maxime PIGEON <maxpige@gmail.com>
2023-04-03 00:19:22 +0200

    fix pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,71 +1,30 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ManifestOptions, VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
-import replace from '@rollup/plugin-replace';
+import { defineConfig } from 'vite';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
 const pwaOptions: Partial<VitePWAOptions> = {
-  mode: 'development',
-  base: '/',
-  includeAssets: ['favicon.svg'],
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico'],
   manifest: {
-    name: 'PWA Router',
-    short_name: 'PWA Router',
-    theme_color: '#ffffff',
+    name: 'Oseille',
+    short_name: 'Oseille',
     icons: [
-      {
-        src: 'pwa-192x192.png', // <== don't add slash, for testing
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/pwa-512x512.png', // <== don't remove slash, for testing
-        sizes: '512x512',
-        type: 'image/png',
-      },
-      {
-        src: 'pwa-512x512.png', // <== don't add slash, for testing
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable',
-      },
+      { src: '/favicon/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { src: '/favicon/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
-  },
-  devOptions: {
-    enabled: process.env.SW_DEV === 'true',
-    /* when using generateSW the PWA plugin will switch to classic */
-    type: 'module',
-    navigateFallback: 'index.html',
+    theme_color: '#ffffff',
+    background_color: '#ffffff',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
   },
 };
 
-const replaceOptions = { __DATE__: new Date().toISOString() };
-const claims = process.env.CLAIMS === 'true';
-const reload = process.env.RELOAD_SW === 'true';
-const selfDestroying = process.env.SW_DESTROY === 'true';
-
-if (process.env.SW === 'true') {
-  pwaOptions.srcDir = 'src';
-  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts';
-  pwaOptions.strategies = 'injectManifest';
-  (pwaOptions.manifest as Partial<ManifestOptions>).name = 'PWA Inject Manifest';
-  (pwaOptions.manifest as Partial<ManifestOptions>).short_name = 'PWA Inject';
-}
-
-if (claims) pwaOptions.registerType = 'autoUpdate';
-
-if (reload) {
-  // @ts-expect-error just ignore
-  replaceOptions.__RELOAD_SW__ = 'true';
-}
-
-if (selfDestroying) pwaOptions.selfDestroying = selfDestroying;
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    sourcemap: process.env.SOURCE_MAP === 'true',
-  },
-  plugins: [react(), VitePWA(pwaOptions), replace(replaceOptions)],
+  base: './',
+  plugins: [react(), VitePWA(pwaOptions)],
   define: {
     global: 'window',
   },
