cd4d2da522f7fefa6b1bf228e859e45a39789c07
Maxime PIGEON <maxpige@gmail.com>
2023-04-07 14:43:41 +0200

    pwa now

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,7 @@ const pwaOptions: Partial<VitePWAOptions> = {
   manifest: {
     name: 'Oseille',
     short_name: 'Oseille',
-    icons: [
-      { src: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
-      { src: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
-      { type: 'image/png', sizes: '32x32', src: '/favicon-32x32.png' },
-      { type: 'image/png', sizes: '180x180', src: '/apple-touch-icon.png' },
-      { type: 'image/png', sizes: '16x16', src: '/favicon-16x16.png' },
-    ],
+    icons: [{ src: '/favicon.ico', sizes: 'any', type: 'image/svg+xml' }],
     theme_color: '#ffffff',
     background_color: '#ffffff',
     display: 'standalone',
@@ -26,6 +20,7 @@ const pwaOptions: Partial<VitePWAOptions> = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), VitePWA(pwaOptions)],
   define: {
     global: 'window',
