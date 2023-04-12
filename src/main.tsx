b21c87c9a9db58175eafab98e52dc86bcc8e6999
Maxime PIGEON <maxpige@gmail.com>
2023-04-12 22:27:54 +0200

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { Analytics } from '@vercel/analytics/react';
 import './backend';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import { inject } from '@vercel/analytics';
 
+inject();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
         <App />
-        <Analytics />
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
