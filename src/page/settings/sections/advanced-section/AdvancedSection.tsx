bed2e708c442c1b758b432dfca80c62546a9a978
Maxime PIGEON <maxpige@gmail.com>
2023-09-12 00:04:45 +0200

    remove unnecessary code

--- src/page/settings/sections/advanced-section/AdvancedSection.tsx
+++ src/page/settings/sections/advanced-section/AdvancedSection.tsx
@@ -1,11 +1,5 @@
 import { Flex } from '@chakra-ui/react';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-import { useSnapshot } from 'valtio';
 import { z } from 'zod';
-import { store, updateFarm } from '../../../../backend';
-import { useFarmParameters } from '../../../../utils/hooks/useFarmParameters';
 import { DangerSettingBlock } from './blocks/DangerSettingBlock';
 import { DocumentSettingBlock } from './blocks/DocumentSettingBlock';
 import { ImportExportSettingBlock } from './blocks/ImportExportSettingBlock';
@@ -21,25 +15,6 @@ interface DocumentIdInput {
 }
 
 export function AdvancedSection() {
-  const snap = useSnapshot(store);
-  const { farm } = useFarmParameters();
-
-  const { control, formState, handleSubmit, reset } = useForm<DocumentIdInput>({
-    resolver: zodResolver(documentsSchema),
-    defaultValues: {
-      invoiceId: farm?.invoiceId,
-      deliveryId: farm?.deliveryId,
-    },
-  });
-
-  useEffect(() => {
-    reset({
-      invoiceId: farm?.invoiceId,
-      deliveryId: farm?.deliveryId,
-    });
-  }, [farm]);
-
-  const onSubmit = (e: DocumentIdInput) => farm && updateFarm({ ...farm, ...e });
   return (
     <Flex
       gap={6}
