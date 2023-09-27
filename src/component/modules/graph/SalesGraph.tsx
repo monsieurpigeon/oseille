b267001444c5e8261e7db16d0912fe333ddefc5a
SebastienANTICOURT <sebastien.anticourt@live.fr>
2023-09-27 14:15:29 +0200

    Bar chart montants des factures ts les mois

--- src/component/modules/graph/SalesGraph.tsx
+++ src/component/modules/graph/SalesGraph.tsx
@@ -1,11 +1,13 @@
 import { Chart, registerables } from 'chart.js';
 import 'chartjs-adapter-moment';
 import moment from 'moment';
+import 'moment/locale/fr';
 import { useEffect, useRef } from 'react';
 import { useSnapshot } from 'valtio';
 import { Customer, store } from '../../../backend';
 import { getInvoicePrice } from '../../../utils/aggregations';
 import { priceFormatter } from '../../../utils/formatter';
+moment.locale('fr');
 
 interface SalesGraphProps {
   customer: Customer;
@@ -20,41 +22,44 @@ export function SalesGraph({ customer }: SalesGraphProps) {
   const customerInvoice = store.invoices.filter((invoice) => invoice.customerId === customer.id);
 
   const total = customerInvoice.reduce((acc, invoice) => acc + getInvoicePrice(invoice), 0);
-  const momentDate = moment('15/08/2023', 'DD/MM/YYYY');
+
   useEffect(() => {
     if (chartRef.current && customerInvoice.length > 0) {
-      const labels = customerInvoice.map((invoice) =>
-        moment(new Date(invoice.createdAt).toLocaleDateString(), 'DD/MM/YYYY'),
-      );
-      const data = customerInvoice.map((invoice) => getInvoicePrice(invoice));
+      const aggregatedData = customerInvoice.reduce<{ [key: string]: number }>((acc, invoice) => {
+        const month = moment(invoice.createdAt).locale('fr').format('MMM YYYY');
+        acc[month] = (acc[month] || 0) + getInvoicePrice(invoice);
+        return acc;
+      }, {});
+
+      const labels = Object.keys(aggregatedData);
+      const data = Object.values(aggregatedData);
 
       const chart = new Chart(chartRef.current, {
-        type: 'line',
+        type: 'bar',
         data: {
           labels: labels,
           datasets: [
             {
-              label: 'Montant de la Facture',
+              label: 'Montant de la Facture par Mois',
               data,
+              backgroundColor: 'rgba(75, 192, 192, 0.6)',
               borderColor: 'rgba(75, 192, 192, 1)',
               borderWidth: 1,
-              backgroundColor: 'rgba(75, 192, 192, 0.2)',
             },
           ],
         },
         options: {
           scales: {
             x: {
-              type: 'time',
-              time: {
-                unit: 'month',
-                displayFormats: {
-                  month: 'MMM YYYY',
-                },
-              },
+              type: 'category',
             },
             y: {
               beginAtZero: true,
+              ticks: {
+                callback: function (value) {
+                  return value + ' €';
+                },
+              },
             },
           },
         },
